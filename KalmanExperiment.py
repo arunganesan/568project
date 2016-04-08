a57ce1b241bf1886cea12e77fe1f5bbe20f17529
shurjo <shurjo@umich.edu>
2016-04-08 22:33:17 +0000

    working update step

--- KalmanExperiment.py
+++ KalmanExperiment.py
@@ -9,6 +9,11 @@ sys.path.append('/home/pi/Documents/dev/diddyborg-tabletop/sensors/IMU_LSM303/Gy
 # Run the EKF
 from filterpy.common import Q_discrete_white_noise
 from filterpy.kalman import ExtendedKalmanFilter
+
+# Use below if you want to debug the EKF filter code.
+# But actually, probably that code is working well
+#from EKF import ExtendedKalmanFilter
+
 from numpy import eye, array, asarray
 import numpy as np
 import time
@@ -23,12 +28,14 @@ def printStuff(rk):
 
 def HJacobian_at(x, landmarkPosition):
     """ compute Jacobian of H matrix for state x """
-
+   
     dx = landmarkPosition[0] - x[0]
     dy = landmarkPosition[1] - x[1]
     q  = dx**2 + dy**2
-    H  =  np.array ([[-dx/sqrt(q), -dy/sqrt(q),  0, ],
-                     [ dy/q      , -dx/q      , -1, ]]) 
+    # H  =  np.array ([[-dx/sqrt(q), -dy/sqrt(q),  0, ],
+    #                  [ dy/q      , -dx/q      , -1, ]]) 
+    
+    H  =  np.array ([[ (dy/q)[0]      , (-dx/q)[0]      , -1,  0,  0]]) 
     # print H.shape
     return H
 
@@ -38,8 +45,12 @@ def hx(x, landmarkPosition):
     dx = landmarkPosition[0] - x[0]
     dy = landmarkPosition[1] - x[1]
     q  = dx**2 + dy**2 
-    h  = np.array([sqrt(q), minimizedAngle(math.atan2(dy, dx) - x[2])])  
+    #h  = np.array([sqrt(q), minimizedAngle(math.atan2(dy, dx) - x[2])])  
+    #h = np.array([[minimizedAngle(math.atan2(dy, dx) - x[2])]])
+    h = np.array([minimizedAngle(math.atan2(dy, dx) - x[2])])
     # print h
+
+    #print 'in kal.', h.shape
     return h
 
 def predict_State(rk, dt):
@@ -55,7 +66,7 @@ def predict_State(rk, dt):
     #rk.x = np.dot(rk.F, rk.x) + np.array([.5*a_x*dt*dt, .5*a_y*dt*dt, omega])
 
     # Account for zero velocities
-    rk.x = np.dot(rk.F, rk.x) + np.array([.5*a_x*dt*dt, .5*a_y*dt*dt, omega*dt/14.375, a_x*dt, a_y*dt])
+    rk.x = np.dot(rk.F, rk.x) + np.array([[.5*a_x*dt*dt, .5*a_y*dt*dt, omega*dt/14.375, a_x*dt, a_y*dt]]).T
     #print a_y, dt
     #rk.x[0] = rk.x[0] + .5*a_x*dt*dt;
     #rk.x[1] = rk.x[1] + .5*a_y*dt*dt;
@@ -78,7 +89,7 @@ from measure import *
 dt = .01 # TBD
 
 # Initialize Extended Kalman Filter
-rk = ExtendedKalmanFilter(dim_x=5, dim_z=2)
+rk = ExtendedKalmanFilter(dim_x=5, dim_z=1)
 
 # Initialize IMU
 imu = IMU()
@@ -93,7 +104,7 @@ measure = Measure(debug_mode=False)
 # Make an imperfect sta rting guess
 #rk.x = array([0, 0 , 0]) #x, y, theta, v_x, v_y
 
-rk.x = array([0, 0 , 0, 0, 0]) #x, y, theta, v_x, v_y
+rk.x = array([[0, 0 , 0, 0, 0]]).T #x, y, theta, v_x, v_y
 """
 rk.F = array([[1, 0, 0],
               [0, 1, 0],
@@ -106,7 +117,8 @@ range_std = .01 # metersi
 #rk.Q = np.diag([range_std**2, range_std**2, range_std**2 ])
 rk.Q = np.diag([range_std**2, range_std**2, range_std**2,  range_std**2, range_std**2])
 # Measurement Noise
-rk.R = np.diag([range_std**2, range_std**2])
+#rk.R = np.diag([range_std**2, range_std**2])
+rk.R = np.array([[range_std**2]])
 
 # This might imply correlation which in our case is unfounded
 #rk.Q[0:2, 0:2] = Q_discrete_white_noise(2, dt=dt, var=0.1)
@@ -210,6 +222,8 @@ try:
             z = np.array([zm['bearing']])
             markerId = zm['id']
             landmarkPosition = get_landmark(markerId)
+            
+            
             rk.update(z, HJacobian_at, hx, args=landmarkPosition, hx_args=landmarkPosition)
         
         #printStuff(rk)
