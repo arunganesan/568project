d23ed353bf629c39fdbd997d2f57f39ae333bb84
Arun <arunganesan123@gmail.com>
2016-04-09 20:45:58 -0400

    test script added

---
+++ testcamera.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+from measure import *
+from flow import *
+
+if __name__ == '__main__':
+  import time
+
+  m = Measure()
+  f = Flow()
+
+  for i in range(30):
+    measurements = m.get_measurement()
+    motions = m.get_motions
+    print 'T={0}, Measurements={1}, Motions={2}'.format(i, measurements, motions)
+    time.sleep(0.25)
+  m.kill()
