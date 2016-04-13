7abc72379e73e2c3eca1e398b5c93b83527b57cf
Arun <arunganesan123@gmail.com>
2016-04-13 15:42:21 -0400

    detect in three slightly modified

--- stitching/detect_inthree.py
+++ stitching/detect_inthree.py
@@ -6,27 +6,59 @@ import glob
 #files = glob.glob('image_*.png')
 
 groundtruth=50
-files=glob.glob('images/smaller/image*.png')
+#files=glob.glob('images/smaller/image*.png')
 
 #groundtruth = 36
 #files = glob.glob('images/larger/image*.png')
 
 groundtruth=36
 #files=glob.glob('images/newfullres/fullres*.png')
-files=glob.glob('images/newfullres/image*.png')
+files_door =glob.glob('images/frommemory/door*.png')
+files_center = glob.glob('images/frommemory/center*.png')
+files_wall = glob.glob('images/frommemory/wall*.png')
+
+allfiles = zip(files_door, files_center, files_wall)
 
 total_detected = 0
-for fname in files:
-  p = subprocess.Popen(['./coordinates', fname], stdout=subprocess.PIPE)
+for door, center, wall in allfiles:
+  
+  p = subprocess.Popen(['./coordinates', door], stdout=subprocess.PIPE)
   txt = p.communicate()[0]
-  detected = None
+  detected_door = None
   for line in txt.split('\n'):
     if line.strip() == '': continue
     parts = line.strip().split()
     if int(parts[2]) == groundtruth:
-      detected = line 
+      detected_door = line 
       break
-  print fname, detected
-  if detected: total_detected += 1
-
-print 'Detected {}/{}'.format(total_detected, len(files))
+  
+  p = subprocess.Popen(['./coordinates', center], stdout=subprocess.PIPE)
+  txt = p.communicate()[0]
+  detected_center = None
+  for line in txt.split('\n'):
+    if line.strip() == '': continue
+    parts = line.strip().split()
+    if int(parts[2]) == groundtruth:
+      detected_center = line 
+      break
+  
+  
+  p = subprocess.Popen(['./coordinates', wall], stdout=subprocess.PIPE)
+  txt = p.communicate()[0]
+  detected_wall = None
+  for line in txt.split('\n'):
+    if line.strip() == '': continue
+    parts = line.strip().split()
+    if int(parts[2]) == groundtruth:
+      detected_wall = line 
+      break
+  
+  
+  if detected_door is None and detected_center is None and detected_wall is None:
+    detected = False
+  else:
+    detected = True
+    total_detected += 1
+  
+  print door, detected
+print 'Detected {}/{}'.format(total_detected, len(allfiles))
